5d97ff93683bde737d884648a8dfd286c86d9297
raplordrds <habib.hakeem@yahoo.com>
2023-11-06 20:29:18 +0000

    successfully generated questions to show on page after hiding the start screen and set a timer using set time interval and appending it to page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,13 +1,52 @@
+var startScreen = document.getElementById('start-screen');
 var startButton = document.getElementById('start');
-var startScreen = document.getElementById('start-screen')
-var bodyEl = document.querySelector('body')
-var questionContainer = document.getElementById("bodyWrapper")
+var questionsDiv = document.getElementById('questions');
+var questionTitle = document.getElementById("question-title");
+var choicesContainer = document.getElementById('choices');
+var endScreen = document.getElementById('end-screen');
+var finalScore = document.getElementById('final-score');
+var initialsInput = document.getElementById('initials');
+var submitInitialsButton = document.getElementById('submit');
+var feedback = document.getElementById("feedback");
+var timeElement = document.getElementById("time")
+
+function generateQuestion() {
+    questionTitle.textContent = quizQuestions[0].question;
+    for(i=0; i<quizQuestions[0].choices.length; i++){
+        let button = document.createElement("button");
+        button.textContent = quizQuestions[0].choices[i];
+        choicesContainer.append(button)
+    }
+    
+}
+
+function startTimer(){
+    var count = 5;
+    var timer = setInterval(function(){
+        count--;
+        timeElement.textContent = count
+        console.log(timer);
+
+        if(count === 0) {
+            clearInterval(timer)
+        }
+
+    }, 1000);
+}
 
 function startQuiz() {
+    // hide the start screen to show questions instead
     startScreen.style.display = 'none';
-    var questions = document.createElement('h2')
-    questions.textContent = quizQuestions[0]
-    questionContainer.append(questions)
+    
+    // start the timer
+    startTimer()
+    
+    // display question container
+    questionsDiv.style.display = 'block';
+    generateQuestion()
+    
+
+    // display first question
 }
 
 startButton.addEventListener('click', startQuiz);
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -4,32 +4,24 @@ var quizQuestions = [
     {
         question: "Commonly used data types DO NOT INCLUDE:",
         choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-        correctAnswer: choices[2]
+        correctAnswer: this.choices[2]
     },
 
     {
         question: "The condition in an if else statement in js is enclosed within what?",
         choices: ["1. Quotes", "2. Parenthesis", "3. Brackets", "4. Curly Brackets"],
-        correctAnswer: choices[1]
+        correctAnswer: this.choices[1]
     },
 
     {
-        question: "Commonly used data types DO NOT INCLUDE:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        correctAnswer: choices[2]
+        question: "VS code can be used to code HTML, CSS and JavaScript",
+        choices: ["1. True", "2. False", "3. Dunno", "4. Maybe"],
+        correctAnswer: this.choices[0]
     },
 
     {
-        question: "Commonly used data types DO NOT INCLUDE:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        correctAnswer: choices[2]
+        question: "A very useful tool used during development and debugging for printing content to the console is:",
+        choices: ["1. JavaScript", "2. Terminal/Bash", "3. For loops", "4. Console.log"],
+        correctAnswer: this.choices[3]
     },
-
-    {
-        question: "Commonly used data types DO NOT INCLUDE:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        correctAnswer: choices[2]
-    }
-]
-console.log(startButton);
-
+];
